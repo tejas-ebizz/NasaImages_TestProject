eeac45906136478a3b83063fc21cb8eca1677011
Tejas Patel <tejas.ebizzinfotech@gmail.com>
2023-03-06 16:39:27 +0530

    Require Pods Added
    - Added require pods and change in project structure.

---
+++ NasaImages TestProject/Constants/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  NasaImages TestProject
+//
+//  Created by TL-1 on 06/03/23.
+//
+
+import Foundation
+
+//MARK: API URL
+//let MAIN_API = "https://raw.githubusercontent.com/obvious/take-home-exercise-data/trunk/nasa-pictures.json"
+let MAIN_API = "https://dummyjson.com/products"
---
+++ NasaImages TestProject/Helper/APIManager.swift
@@ -0,0 +1,9 @@
+//
+//  APIManager.swift
+//  NasaImages TestProject
+//
+//  Created by TL-1 on 06/03/23.
+//
+
+import Foundation
+
