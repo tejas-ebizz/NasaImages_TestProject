e65d8aabd310cf2f251cd796bd3b9a4aca32f777
Tejas Patel <tejas.ebizzinfotech@gmail.com>
2023-03-06 18:47:01 +0530

    Added Testcases
    - Added testcases (Unit testcases)
    - Also added UI testing.

--- NasaImages TestProjectUITests/NasaImages_TestProjectUITests.swift
+++ NasaImages TestProjectUITests/NasaImages_TestProjectUITests.swift
@@ -10,32 +10,64 @@ import XCTest
 final class NasaImages_TestProjectUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+     
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+   
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
+        // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
+    
+    func testAppFlow(){
+        // if we didnt get api response in 10 second case will fail
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10 , execute: { // waiting for api call
+            let app = XCUIApplication()
+            
+            let element = app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+            element.children(matching: .other).element(boundBy: 0).collectionViews.cells.otherElements.containing(.staticText, identifier:"iPhone 9").element.tap()
+            
+            let icBackButton = app.buttons["ic back"]
+            icBackButton.tap()
+            
+            let cellsQuery = app.collectionViews.cells
+            cellsQuery.otherElements.containing(.staticText, identifier:"MacBook Pro").element.swipeUp()
+            element.children(matching: .other).element.children(matching: .collectionView).element.swipeDown()
+            cellsQuery.otherElements.containing(.staticText, identifier:"iPhone X").element.tap()
+            icBackButton.tap()
+        })
+        
+        
+    }
+    func testAppFailedFlow(){
+        // not waiting for api call so this case will always fail
+            let app = XCUIApplication()
+            let element = app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+            element.children(matching: .other).element(boundBy: 0).collectionViews.cells.otherElements.containing(.staticText, identifier:"iPhone 9").element.tap()
+            
+            let icBackButton = app.buttons["ic back"]
+            icBackButton.tap()
+            
+            let cellsQuery = app.collectionViews.cells
+            cellsQuery.otherElements.containing(.staticText, identifier:"MacBook Pro").element.swipeUp()
+            element.children(matching: .other).element.children(matching: .collectionView).element.swipeDown()
+            cellsQuery.otherElements.containing(.staticText, identifier:"iPhone X").element.tap()
+            icBackButton.tap()
+       
     }
 }
