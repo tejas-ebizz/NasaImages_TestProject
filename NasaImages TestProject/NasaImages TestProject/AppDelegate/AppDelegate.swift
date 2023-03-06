3ac08f24cac211b620146607008516e56a130969
Tejas Patel <tejas.ebizzinfotech@gmail.com>
2023-03-06 16:25:03 +0530

    Initial Project Setup
    - Make require changes and setup initial project

---
+++ NasaImages TestProject/NasaImages TestProject/AppDelegate/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  NasaImages TestProject
+//
+//  Created by TL-1 on 06/03/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
